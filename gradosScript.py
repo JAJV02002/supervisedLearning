53ec77de371af90f99afed3e79ca6eddedd0ac18
JOSE ANTONIO JUAREZ VELAZQUEZ <54070531+JAJV02002@users.noreply.github.com>
2024-04-25 18:46:18 -0600

    Now showing the data according to the question

---
+++ gradosScript.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as mtp
+from sklearn.linear_model import LinearRegression
+
+# Read the CSV file
+datos = pd.read_csv("grados.csv")
+datos.info()
+datos.head()
+
+sb.scatterplot(x="celsius", y="fahrenheit", data=datos)
+mtp.show()
+
+x = datos["celsius"]
+y = datos["fahrenheit"]
+print(x)
+print(y)
+
+
+x_procesada = x.values.reshape(-1, 1)
+print(x_procesada)
+y_procesada = y.values.reshape(-1,1)
+print(y_procesada)
